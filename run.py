694080f8e520b19bddadb61f0bb7c037b8b0b7ac
tracktor1 <39525540+tracktor1@users.noreply.github.com>
2019-05-20 13:46:58 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,18 +6,33 @@ from func import *
 
 apt_update = 'update'   # update repository
 apt_nodejs = 'nodejs'
+apt_docker = 'docker-ce'
+
 newhostname = 'host2'   # New hostname
 
 #rename host
 host_ren(newhostname)
 
-# Add nodejs repo
+# Add nodejs repo to APT sources
 os.system('curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -')
 
 # Install nodeJS
 install_apt(apt_nodejs)
 
+
+# add PGP key
+os.system('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -')
+#Add the Docker repo to APT sources
+os.system('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"')
+
 # run apt-get update
-#print ("Updating apt...")
-#install_apt(apt_update)
+print ("Updating apt...")
+install_apt(apt_update)
+
+# Install docker
+install_apt(apt_docker)
+
+
+
+
 
