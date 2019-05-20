2ab46183b63aa239feb8559943c5e86acd80adb4
tracktor1 <39525540+tracktor1@users.noreply.github.com>
2019-05-20 13:36:14 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,14 +5,19 @@ from func import *
 
 
 apt_update = 'update'   # update repository
-
+apt_nodejs = 'nodejs'
 newhostname = 'host2'   # New hostname
 
 #rename host
 host_ren(newhostname)
 
+# Add nodejs repo
+os.system('curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -')
+
+# Install nodeJS
+install_apt(apt_nodejs)
 
 # run apt-get update
-print ("Updating apt...")
-install_apt(apt_update)
+#print ("Updating apt...")
+#install_apt(apt_update)
 
