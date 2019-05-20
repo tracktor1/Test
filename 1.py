6e9bde825aed7bf8c3a17490647c2095f3a81f6a
tracktor1 <39525540+tracktor1@users.noreply.github.com>
2019-05-20 14:08:10 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,21 @@
 
-import os
+
+import json
+
+jdate = { "year": "2000", "month": "12", "day": "1" }
+jdict = { "content": "hello world", "date": jdate }
 
 
-# Add nodejs repo
-os.system('curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -')
+print (json.dumps(jdict))
+
+
+'''
+print('serialization')
+myDictObj = { "name":"John", "age":30, "car":None }
+##convert object to json
+serialized= json.dumps(myDictObj, sort_keys=True, indent=3)
+print(serialized)
+## now we are gonna convert json to object
+deserialization=json.loads(serialized)
+print(deserialization)
+'''
