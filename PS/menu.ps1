7dfd2bccd3688a9a9e62779445937b700b990483
David <39525540+tracktor1@users.noreply.github.com>
2019-11-02 15:28:02 +0200

    Delete menu.ps1

--- PS/menu.ps1
+++
@@ -1,8 +0,0 @@
-$processes = Get-Process
-$menu = @{}
-for ($i=1;$i -le $processes.count; $i++) 
-{ Write-Host "$i. $($processes[$i-1].name)"
-$menu.Add($i,($processes[$i-1].name)) }
-
-[int]$ans = Read-Host 'Enter selection'
-$selection = $menu.Item($ans) ; Get-Process $selection
